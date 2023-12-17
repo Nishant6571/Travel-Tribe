4d51dec2059bf622a8a5dcf9ab63ffa721750ba8
Nishant6571 <nishantbhargava.bhargava@gmail.com>
2023-12-17 13:05:52 +0530

    update

--- login.js
+++ login.js
@@ -3,33 +3,40 @@ function redirectToPage(pagename) {
   window.location.href = loginPageUrl;
 }
 
-async function login() {
-  const email = document.getElementById("Nemail").value;
-  const password = document.getElementById("Npassword").value;
-
+async function fetchData(url) {
   try {
-    const response = await fetch("https://loginpage-olv8.onrender.com/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
 
-    if (response.ok) {
-      const responseData = await response.json();
-      localStorage.setItem("token", responseData.token);
-      alert("Login successful!");
-      setTimeout(() => {
-        redirectToPage("./Home.html");
-      }, 2000);
-    } else {
-      alert("Login failed. Please check your credentials.");
-    }
+    return data;
   } catch (error) {
-    console.error("Error during login:", error);
-    alert("An unexpected error occurred. Please try again later.");
+    console.error("Error fetching data:", error);
   }
 }
+fetchData();
+async function validateLogin() {
+  const emailInput = document.getElementById("email").value;
+  const passwordInput = document.getElementById("password").value;
 
-document.getElementById("Nlogin-id").addEventListener("click", login);
+  const userData = await fetchData("https://loginpage-olv8.onrender.com/users");
+
+  console.log(passwordInput);
+
+  console.log("Email Input:", emailInput);
+  console.log("Password Input:", passwordInput);
+
+  const user = userData.find(
+    (user) => user.email === emailInput && user.password === passwordInput
+  );
+
+  if (user) {
+    window.location.href = "Home.html";
+  } else {
+    alert("Incorrect email or password. Please try again.");
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  const sign = document.querySelector(".login-button");
+  sign.addEventListener("click", validateLogin);
+});
